3bcd63a99294660e84e08fd51d3892f72de0cab5
Restyled.io <commits@restyled.io>
2021-05-01 16:11:03 +0000

    Restyled by yapf

--- services/model/main.py
+++ services/model/main.py
@@ -105,7 +105,9 @@ def get_score(filename, X):
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    sentence_words = [
+        lemmatizer.lemmatize(word.lower()) for word in sentence_words
+    ]
     return sentence_words
 
 
